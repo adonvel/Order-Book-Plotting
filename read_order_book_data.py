1e91c3e5b88a2e850c43c8315573004eec878c07
adonvel <a.donis.vela@gmail.com>
2019-11-07 13:24:07 +0100

    read file ready and included data files (csv)

--- read_order_book_data.py
+++ read_order_book_data.py
@@ -1,1 +1,10 @@
+# +
 ''' file to read order book data'''
+
+import pandas as pd
+
+df = pd.read_csv(r'/home/learner/Order-Book-Plotting/AAPL_2012-06-21_34200000_37800000_message_50.csv',header=None)
+print(type(df.values))
+# -
+
+
