1c1e45e5c54b3a65c146c7feeae48925d4dc0fb4
BrianPW <brian.paquelet@gmail.com>
2019-11-07 14:18:24 +0100

    order book now gives time and prices over the day

--- read_order_book_data.py
+++ read_order_book_data.py
@@ -2,9 +2,23 @@
 ''' file to read order book data'''
 
 import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
 
-df = pd.read_csv(r'/home/learner/Order-Book-Plotting/AAPL_2012-06-21_34200000_37800000_message_50.csv',header=None)
+""" global variables"""
+N_intervals = 100       # number of intervals between max and min price 
+
+df_m = pd.read_csv(r'/home/learner/Order-Book-Plotting/order_book_depth1/GOOG_2012-06-21_34200000_57600000_message_1.csv',header=None)
 print(type(df.values))
+
+time = df_m.values[: , 0]
+prices = df_m.values[: , 4]
+print(np.amax(prices))
+print(np.amin(prices))
+
+df_o =  pd.read_csv(r'/home/learner/Order-Book-Plotting/order_book_depth1/GOOG_2012-06-21_34200000_57600000_orderbook_1.csv',header=None)
+#print(df_o([:: , :1]))
+
 # -
 
 
