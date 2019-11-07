1679b0d86ff8550d43e4e1221fe7421af99270fa
adonvel <a.donis.vela@gmail.com>
2019-11-07 16:53:38 +0100

    Plotting price evolution implemented

--- read_order_book_data.py
+++ read_order_book_data.py
@@ -17,22 +17,35 @@ df_o =  pd.read_csv(r'/home/learner/Order-Book-Plotting/order_book_depth10/AMZN_
 t = df_m.values[: , 0]
 t_steps = len(t)
 prices = df_m.values[: , 4]
-levels = len(df_o.values[i,0::4])
+order_type = df_m.values[: , 1]
+
+
+market_price = prices[np.where(order_type == 4) or np.where(order_type == 5)]/10000
+time_price = t[np.where(order_type == 4) or np.where(order_type == 5)]/10000
+plt.plot(time_price,market_price)
+
+
+plt.plot(time_price,market_price)
+
+
+# +
+levels = len(df_o.values[0,0::4])
 cumulative_vol_buy = np.zeros((levels,))
 cumulative_vol_sell = np.zeros((levels,))
 
+
 for i in range(20):
-    buy_price = df_o.values[i,0::4]
+    buy_price = df_o.values[i,0::4]/10000
     buy_amount = df_o.values[i,1::4]
-    sell_price = df_o.values[i,2::4]
+    sell_price = df_o.values[i,2::4]/10000
     sell_amount = df_o.values[i,3::4]
     
     for j in range(levels):
         cumulative_vol_buy[j] =  np.sum(buy_amount[0:j+1])
         cumulative_vol_sell[j] =  np.sum(sell_amount[0:j+1])
     
-    plt.plot(buy_price,buy_amount)
-    plt.plot(sell_price,sell_amount)
+    #plt.plot(buy_price,buy_amount)
+    #plt.plot(sell_price,sell_amount)
     plt.plot(buy_price,cumulative_vol_buy)
     plt.plot(sell_price,cumulative_vol_sell)
     plt.show()
@@ -42,13 +55,7 @@ for i in range(20):
 
 sell_orders = np.zeros((N_intervals,t_steps))
 buy_orders = np.zeros((N_intervals,len(t)))
-
-# Set initial state
-#for i in
-
-
 # -
-del time
 
 
 
