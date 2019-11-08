01084b9960cad9fcf123097a4ba66d92f26fa56f
adonvel <a.donis.vela@gmail.com>
2019-11-08 14:14:11 +0100

    presented version

--- read_order_book_data_v2.py
+++ read_order_book_data_v2.py
@@ -10,8 +10,8 @@ from IPython import display
 
 # Save data in DATAFRAME--------------------------------------------------------------
 
-df_m = pd.read_csv(r'/home/learner/Order-Book-Plotting/order_book_depth10/AMZN_2012-06-21_34200000_57600000_message_10.csv',header=None)
-df_o =  pd.read_csv(r'/home/learner/Order-Book-Plotting/order_book_depth10/AMZN_2012-06-21_34200000_57600000_orderbook_10.csv',header=None)
+df_m = pd.read_csv(r'/home/learner/Order-Book-Plotting/order_book_depth30/MSFT_2012-06-21_34200000_37800000_message_30.csv',header=None)
+df_o =  pd.read_csv(r'/home/learner/Order-Book-Plotting/order_book_depth30/MSFT_2012-06-21_34200000_37800000_orderbook_30.csv',header=None)
 
 
 # +
@@ -21,60 +21,72 @@ t = df_m.values[: , 0]
 t_steps = len(t)
 prices = df_m.values[: , 4]
 order_type = df_m.values[: , 1]
+w=10
 
 
 market_price = prices[np.where(order_type == 4) or np.where(order_type == 5)]/10000
 time_price = t[np.where(order_type == 4) or np.where(order_type == 5)]
 
-for k in range(t_steps//50):
+for k in range(100):
+    plt.clf()
     axes = plt.gca()
-    #axes.set_xlim([np.amin(market_price),np.amax(market_price)])
-    pl.plot(time_price[0:1000+10*k],market_price[0:1000+10*k])
+    plt.title('Evolution of Microsoft stock', size=16)
+    plt.ylabel('Price (USD)')
+    plt.xlabel('Time (minutes after 9:30)')
+    pl.plot((time_price[0:1000+w*k]-34000)/60,market_price[0:1000+w*k])
 
-    #time.sleep(0.001)
     display.display(pl.gcf())
     display.clear_output(wait=True)  
-    plt.clf()
-
-
+    
 
 
 # +
-#Plot order book cumulative volume----------------------------------------
+#Plot order book cumulative order depth----------------------------------------
 
 levels = len(df_o.values[0,0::4])
 t_steps = len(t)
 cumulative_vol_buy = np.zeros((levels,))
 cumulative_vol_sell = np.zeros((levels,))
+v = 500
 
 
 plt.Figure()
-#axes.set_ylim([ymin,ymax])
 
-for i in range(t_steps):
-    buy_price = df_o.values[i,0::4]/10000
-    buy_amount = df_o.values[i,1::4]
-    sell_price = df_o.values[i,2::4]/10000
-    sell_amount = df_o.values[i,3::4]
+for i in range(100):
+    
+    #Load data in arrays
+    
+    plt.clf()
+    buy_price = df_o.values[v*i,0::4]/10000
+    buy_amount = df_o.values[v*i,1::4]
+    sell_price = df_o.values[v*i,2::4]/10000
+    sell_amount = df_o.values[v*i,3::4]
+    
+    #Integrate to obtain the cumulative order depth
     
     for j in range(levels):
         cumulative_vol_buy[j] =  np.sum(buy_amount[0:j+1])
         cumulative_vol_sell[j] =  np.sum(sell_amount[0:j+1])
     
-    
-    #pl.plot(buy_price,buy_amount)
-    #pl.plot(sell_price,sell_amount)
+    #Plot
     axes = plt.gca()
-    axes.set_xlim([np.amin(market_price),np.amax(market_price)])
+    axes.set_xlim([0.995*np.amin(market_price),1.005*np.amax(market_price)])
+    axes.set_ylim([0,350000])
+    plt.title('Order book evolution',size=16)
+    plt.ylabel('Cumulative order depth (shares $\cdot $ USD) ')
+    plt.xlabel('Price (USD)')
     pl.plot(buy_price,cumulative_vol_buy)
     pl.plot(sell_price,cumulative_vol_sell)
-
+    
+    # textstr = '\n'.join(('text'))
+    # pl.text(0, 0, textstr)
+    
     time.sleep(0.001)
     display.display(pl.gcf())
     display.clear_output(wait=True)  
-    plt.clf()
+    
 
 # -
-del t_steps
+
 
 
