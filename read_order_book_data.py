158524e90bad886bffc9c1d14c230100bef85fc7
adonvel <a.donis.vela@gmail.com>
2019-11-08 10:44:08 +0100

    Clear structure

--- read_order_book_data.py
+++ read_order_book_data.py
@@ -6,16 +6,17 @@ import matplotlib.pyplot as plt
 import numpy as np
 import time
 
-# global variables
-N_intervals = 10       # number of intervals between max and min price 
+#------Save files in dataframe-----------------------------------------------------------------
 
 df_m = pd.read_csv(r'/home/learner/Order-Book-Plotting/order_book_depth10/AMZN_2012-06-21_34200000_57600000_message_10.csv',header=None)
 df_o =  pd.read_csv(r'/home/learner/Order-Book-Plotting/order_book_depth10/AMZN_2012-06-21_34200000_57600000_orderbook_10.csv',header=None)
 
 
 # +
+#-------Get price evolution--------------------------------------------------------------------
+
 t = df_m.values[: , 0]
-#t_steps = len(t)
+t_steps = len(t)
 prices = df_m.values[: , 4]
 order_type = df_m.values[: , 1]
 
@@ -32,9 +33,9 @@ plt.plot(time_price,market_price)
 levels = len(df_o.values[0,0::4])
 cumulative_vol_buy = np.zeros((levels,))
 cumulative_vol_sell = np.zeros((levels,))
+# #%matplotlib widget
 
-
-for i in range(20):
+for i in range(5):
     buy_price = df_o.values[i,0::4]/10000
     buy_amount = df_o.values[i,1::4]
     sell_price = df_o.values[i,2::4]/10000
@@ -43,17 +44,26 @@ for i in range(20):
     for j in range(levels):
         cumulative_vol_buy[j] =  np.sum(buy_amount[0:j+1])
         cumulative_vol_sell[j] =  np.sum(sell_amount[0:j+1])
-    
-    #plt.plot(buy_price,buy_amount)
-    #plt.plot(sell_price,sell_amount)
-    plt.plot(buy_price,cumulative_vol_buy)
-    plt.plot(sell_price,cumulative_vol_sell)
-    plt.show()
-    time.sleep(0.33)
-    plt.clf()
-    
+        
+    if j==0:
+        fig = None
+        
+    plt.close(fig)
+   
+    fig = plt.figure()
+    ax = fig.add_subplot()
+    ax.plot(buy_price,cumulative_vol_buy)
+    ax.plot(sell_price,cumulative_vol_sell)
+    #ax.plot(buy_price,buy_amount)
+    #ax.plot(sell_price,sell_amount)
+    #plt.show()
+
+    time.sleep(1)
+
 
 # -
-del t_steps
+
+
+
 
 
